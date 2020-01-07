302db21e0463cb4d17d21188e019ac839bf0ee4e
gothhouse <jamiedang1349@gmail.com>
2020-01-07 09:01:36 -0800

    Adding rigidbody/script to player

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    //Variables
+    public float movementSpeed;
+
+
+
+    //Functions
+    private void Update()
+    {
+        
+
+    }
+}
